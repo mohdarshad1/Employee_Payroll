8652875eb23b139ca0a06978c47f9cb91b8d3c29
mohdarshad1 <mdarshad181@gmail.com>
2021-05-13 16:28:49 +0530

    [Mohd Arshad] : Add Create Database

---
+++ Employee_Payroll.sql
@@ -0,0 +1,28 @@
+#Welcome to Employee Payroll
+
+#UC1
+
+mysql> create database payroll_service;
+
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| mysql              |
+| payroll_service    |
+| performance_schema |
+| sakila             |
+| sys                |
+| world              |
++--------------------+
+
+mysql> use payroll_service
+Database changed
+
+mysql> select database();
++-----------------+
+| database()      |
++-----------------+
+| payroll_service |
++-----------------+
